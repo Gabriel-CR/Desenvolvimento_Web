1d263cecab991f1d2d3d1bce591bb445f65d43d6
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2023-04-04 17:06:43 -0300

    up

--- 005/comunicacao-pai-filho/src/App.js
+++ 005/comunicacao-pai-filho/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Pai from "./components/Pai";
 
 function App() {
   return (
     <div className="App">
       <h1>Comunicação Pai e Filho</h1>
+      <Pai />
     </div>
   );
 }
